4a109dd66e941183ecc95368527d477eb95da520
kiriklzz <kirill.zabrodin03@gmail.com>
2023-11-30 00:02:21 +0300

    code w\o readme

--- backend/cfgg.py
+++ backend/cfgg.py
@@ -1,2 +1,3 @@
 class Config:
     UNSPLASH_ACCESS_KEY = 'x5jhkIPp3XDaNydNbR78TfJ9A6UtIA9hApsKhMaJSgg'
+    #в .gitignore добавил, git rm -r —cached .git add .git commit -m "clear git cache"git push тоже не помог
